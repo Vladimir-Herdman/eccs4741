6ca9e3726189921a045e3fb70dfa78fb2b0b9ed0
Vladimir Herdman <vovaherdman@gmail.com>
2026-02-23 14:35:05 -0500

    base update: reading from other nodes

--- base.ino
+++ base.ino
@@ -6,6 +6,7 @@ struct XbeeNode {
   char lastmsg[20];
   bool recieved_new_msg = false;
   bool state = false; //assume all off at first, until told otherwise. So, use this to determine if fan on/off, and send msg if need to change state, not every time
+  float val = 0;
 };
 
 SoftwareSerial XBee(0, 1);
@@ -28,20 +29,31 @@ void serial_printf(const char* fmt, ...) {
   Serial.print(to_send);
 }
 
-bool append_node(const char c) {
+bool append_node(const char* str) {
+  if (available_nodes_idx == 8) return false; //max num nodes in network
   for (size_t i=0; i<8; i++) {
-    if (available_nodes[i].flag == c) return false;
+    if (available_nodes[i].flag == str[0]) return false;
   }
-  if (available_nodes_idx == 8) return false;
-  available_nodes[available_nodes_idx].flag = c;
+  char* fcol = strchr(str)
+  available_nodes[available_nodes_idx] = (XbeeNode){
+    .flag = c[0],
+    .state = (strncmp(c+1, "on") == 0),
+    .val = atof()
+  };
   ++available_nodes_idx;
   return true;
 }
 
 void save_responding_nodes() {
-  char* bracket_start;
-  while ((bracket_start = strchr(msg, '<')) != NULL) {
-    if (bracket_start[1] != '>') append_node(bracket_start[1]);
+  char* bracket_start = msg;
+  while ((bracket_start = strchr(bracket_start, '<')) != NULL) {
+    char* bracket_end = strchr(msg, '>');
+    if (!bracket_end) break;
+
+    //NOTE: There may be an issue, if a '<>' message is sent, so don't do that, we aren't checking right now
+    *bracket_end = '\0';
+    append_node(&bracket_start[1]);
+    bracket_start = bracket_end + 1;
   }
 }
 
@@ -57,7 +69,7 @@ void get_all_xbee_message() {
   size_t index = 0;
   while (XBee.available() > 0) {
     incoming_byte = XBee.read();
-    if (incoming_byte == '\n') continue;
+    if (incoming_byte == '\n') continue; //parse out newlines
     msg[index] = incoming_byte;
     ++index;
     if (index == 99) break;
@@ -104,52 +116,18 @@ void respond_to_node(const XbeeNode* xbeenode) {
       break;
 
     case 'L':
-      const int ligtlevel = atoi(&xbeenode->lastmsg[1]);
-      if (light >= 200 && xbeenode->state == true) { //bright and light on
+      const int lightlevel = atoi(&xbeenode->lastmsg[1]);
+      if (lightlevel >= 200 && xbeenode->state == true) { //bright and light on
         Serial.print("<Tturn on fan>\n");
         xbeenode->state = true;
       }
-      else if (light < 200 && xbeenode->state == false) { //dark and light off
+      else if (lightlevel < 200 && xbeenode->state == false) { //dark and light off
         Serial.print("<Tturn off fan>\n");
         xbeenode->state = false;
       }
       break;
   }
 }
-
-/*void handle_message_from_node() {
-const char node_flag = msg[0];
-char msgval[6]; //set msg one ahead so it's just the textual value from that node
-memcpy(msgval, &msg[1], 6);
-const float nodeval = (float)atof(msgval); //assumes nodes only sends number values to base
-
-const float lightThreshhold = 200; //switch this number when we know how dark it needs to be
-//bool isDark = false;
-
-switch (node_flag) {
-
-  case 'T':
-    Serial.print("\ntemp val:"); Serial.print(msg);
-    if (nodeval >= 70) Serial.print("\n<Tfan on >");
-    else if (nodeval < 70) Serial.print("\n<Tfan off>");
-    break;
-
-  case 'L':
-    Serial.print("\nlight val:"); Serial.print(msg);
-    //if(nodeval <= lightThreshhold){
-      //isDark = true;
-    //}
-    if(nodeval <= lightThreshhold){
-      Serial.print("\n<Llight on >");
-    }
-    else if (nodeval > lightThreshhold) {
-      //isDark = false;
-      Serial.print("\n<Llight off>");
-    }
-
-    break;
-}
-}*/
 
 void setup() {
   Serial.begin(9600);
@@ -159,7 +137,6 @@ void setup() {
 void loop() {
   if (available_nodes[0] == nullptr) get_available_nodes();
   if (available_nodes[0] == nullptr) return;
-  //get_all_xbee_message();
 
   //Go through each xbee in node list, and request/get data
   for (size_t i=0; i<available_nodes_idx; i++) {
