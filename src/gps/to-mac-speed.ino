d70b23f84c406787edc5c2bc9c08baaa43aea8b7
Vladimir Herdman <vovaherdman@gmail.com>
2026-04-22 12:34:30 -0400

    There were type issues with unsigned long and int.
    Learnt Arduino int is 16 bits, not 32. Bad assumption.

--- src/gps/to-mac-speed.ino
+++ src/gps/to-mac-speed.ino
@@ -7,6 +7,7 @@
 #define passed_ms(ms) curms - lastms >= (ms)
 #define passed_s(s) curms - lastms >= ((s)*1000)
 #define sp Serial.print
+#define MYPI 3.14159265358979323846
 
 const int RXPin = 0, TXPin = 1;
 const uint32_t GPSBaud = 9600;
@@ -22,30 +23,30 @@ struct Location {
 const Location mac = {40.766888, -83.827956};
 const Location jlk = {40.767861, -83.827911};
 Location cur_loc;
-const int total_ms = SECONDS_TOTAL*1000;
-int start_ms = 0;
+const unsigned long total_ms = (unsigned long)SECONDS_TOTAL*1000UL;
+unsigned long start_ms = 0;
 
 unsigned long curms, lastms;
 unsigned long readcount;
 
 //will do distance in meters
 double haversign(const double lat1, const double lat2, const double lng1, const double lng2) {
-  const int R = 6371000;
-  const double phi1 = (lat1*PI)/180;
-  const double phi2 = (lat2*PI)/180;
-  const double dphi = ((lat2-lat1)*PI)/180;
-  const double dlambda = ((lng2-lng1)*PI)/180;
+  const double R = 6371009.0;
+  const double phi1 = (lat1*MYPI)/180;
+  const double phi2 = (lat2*MYPI)/180;
+  const double dphi = ((lat2-lat1)*MYPI)/180;
+  const double dlambda = ((lng2-lng1)*MYPI)/180;
   const double a = pow(sin(dphi/2), 2) + cos(phi1)*cos(phi2)*pow(sin(dlambda/2), 2);
   const double c = 2*atan2(sqrt(a), sqrt(1-a));
   return R * c;
 }
 
 double haversign(const Location loc1, const Location loc2) {
-  const int R = 6371000;
-  const double phi1 = (loc1.lat*PI)/180;
-  const double phi2 = (loc2.lat*PI)/180;
-  const double dphi = ((loc2.lat-loc1.lat)*PI)/180;
-  const double dlambda = ((loc2.lng-loc1.lng)*PI)/180;
+  const double R = 6371009;
+  const double phi1 = (loc1.lat*MYPI)/180;
+  const double phi2 = (loc2.lat*MYPI)/180;
+  const double dphi = ((loc2.lat-loc1.lat)*MYPI)/180;
+  const double dlambda = ((loc2.lng-loc1.lng)*MYPI)/180;
   const double a = pow(sin(dphi/2), 2) + cos(phi1)*cos(phi2)*pow(sin(dlambda/2), 2);
   const double c = 2*atan2(sqrt(a), sqrt(1-a));
   return R * c;
@@ -126,27 +127,32 @@ void loop() {
     //displayInfo();
     //Serial.print("currently read "); Serial.print((unsigned int)readcount); Serial.print(" times\n");
   }
+  const double distance_ms = haversign(jlk.lat, mac.lat, jlk.lng, mac.lng);
+  //const double distance_ms = haversign(jkl, mac);
+  sp(distance_ms, 6); sp("\n");
+
 
   if (passed_s(1) && cur_loc.lat != 0) {
-    const double distance_m = haversign(cur_loc, mac);
-    const int time_left_ms = total_ms - (curms - start_ms);
+    const double distance_m = haversign(cur_loc.lat, mac.lat, cur_loc.lng, mac.lng);
+    const unsigned long time_left_ms = total_ms - (curms - start_ms);
     readcount = 0;
 
-    sp("Current Distance to Mac: "); sp(distance_m); sp("m\n");
-    sp("\tTime left: "); sp(time_left_ms); sp("ms -> "); sp(time_left_ms/1000.0); sp("s\n");
-    sp("\tSpeed to be in time: "); sp(distance_m/(time_left_ms/1000.0)); sp("mps\n\n");
+    sp("Current Distance to Mac: "); sp(distance_m, 6); sp("m\n");
+    sp("\tTime left: "); sp(time_left_ms); sp("ms -> "); sp(time_left_ms/1000.0, 3); sp("s\n");
+    sp("\tSpeed to be in time: "); sp(distance_m/(time_left_ms/1000.0), 2); sp("mps\n\n");
 
     lastms = curms;
   }
 
-  if ((total_ms - ((int)curms - start_ms)) < 0) {
-    sp("Time limit passed to reach Mac\n");
-    sp("\tCurrent distance from Mac: "); sp(haversign(cur_loc, mac)); sp("\n");
-    while (true) //wait forever
-      ;;
-  }
+  //if ((total_ms - (curms - start_ms)) < 0 && start_ms != 0) {
+    //sp("Time limit passed to reach Mac\n");
+    //sp("\tCurrent distance from Mac: "); sp(haversign(cur_loc, mac)); sp("\n");
+    //while (true) //wait forever
+      //;;
+  //}
 
   if (curms > 5000 && gps.charsProcessed() < 10) {
     Serial.println(F("No GPS detected: check wiring."));
+    delay(1);
   }
 }
